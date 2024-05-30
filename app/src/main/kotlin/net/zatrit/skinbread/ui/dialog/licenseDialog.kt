aa46021b8dfd89305e2050939c6b09ac41f79142
zatrit <zatrit@gmail.com>
2024-05-30 10:10:24 +0300

    ui: Optimized text_license

--- app/src/main/kotlin/net/zatrit/skinbread/ui/dialog/licenseDialog.kt
+++ app/src/main/kotlin/net/zatrit/skinbread/ui/dialog/licenseDialog.kt
@@ -3,25 +3,33 @@ package net.zatrit.skinbread.ui.dialog
 import android.app.*
 import android.content.*
 import android.net.Uri
+import android.text.method.ScrollingMovementMethod
 import android.widget.TextView
 import net.zatrit.skinbread.*
 
+
 fun licenseDialog(context: Context, library: Library): Dialog =
-  dialogBuilder(context).apply {
-      setView(R.layout.dialog_license)
-      setTitle(context.resources.getString(R.string.license_of, library.name))
+    dialogBuilder(context).apply {
+        setView(R.layout.dialog_license)
+        setTitle(
+            context.resources.getString(
+                R.string.library_by_author, library.name, library.author
+            )
+        )
 
-      setNeutralButton(R.string.source_code) { _, _ ->
-          val intent = Intent(Intent.ACTION_VIEW, Uri.parse(library.url))
-          context.startActivity(intent)
-      }
+        setNeutralButton(R.string.source_code) { _, _ ->
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(library.url))
+            context.startActivity(intent)
+        }
 
-      setPositiveButton(android.R.string.ok, CancelDialog())
-  }.create().applyDialogTheme().apply {
-      setOnShowListener { dialog ->
-          if (dialog !is AlertDialog) return@setOnShowListener
+        setPositiveButton(android.R.string.ok, CancelDialog())
+    }.create().applyDialogTheme().apply {
+        setOnShowListener { dialog ->
+            if (dialog !is AlertDialog) return@setOnShowListener
 
-          val licenseText = requireViewById<TextView>(R.id.text_license)
-          licenseText.text = context.resources.openRawResource(library.license).reader().readText()
-      }
-  }
+            val licenseText = requireViewById<TextView>(R.id.text_license)
+            licenseText.text =
+                context.resources.openRawResource(library.license).reader()
+                    .readText()
+        }
+    }
